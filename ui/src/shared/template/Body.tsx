6f72f6519e3b8ea7934a58fb30d667e5f5be77a0
hengwangsay <heng.wang@crypto.com>
2022-11-19 22:44:11 +0800

    fix: remove useless file

--- ui/src/shared/template/Body.tsx
+++ ui/src/shared/template/Body.tsx
@@ -1,11 +1,8 @@
-// import { ViteInject } from "./ViteInject";
-
 import type { HTMLProps } from ".";
 
 export const Body = ({ children, script = [] }: HTMLProps) => (
   <body>
     <div id="__content__">{children}</div>
     {script.filter(Boolean).map((ele) => ele)}
-    {/* {__VITE__ && <ViteInject />} */}
   </body>
 );
